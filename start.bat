1596f080bd857983facf76b598579d5bdb9a94e0
Jota <32432079+JotaQC@users.noreply.github.com>
2025-04-21 14:10:40 +0200

    Update start.bat
    Add basic logging to backup run section (logs whether a backup exists and if backup script was executed or skipped. Nothing fancy.)

--- start.bat
+++ start.bat
@@ -52,24 +52,58 @@ cls
 
 :: !!! For this part, it is recommended to have PowerShell 7 installed for compatibility with backup_7z.ps1 !!!
 
-::Get actual date with PowerShell
+:: Enable delayed variable expansion
+setlocal enabledelayedexpansion
+
+:: = Logs =
+::Logs path
+set logsPath=H:\Logs
+
+::Check if Logs directory exist. If not, create it
+if not exist %logsPath% (
+    echo Logs directory does not exist. Creating one . . .
+    mkdir "%logsPath%"
+) else (
+    echo Logs directory already exist. Skipping . . .
+)
+
+::Get current date with PowerShell
 for /f %%i in ('pwsh -Command "Get-Date -Format dd-MM-yyyy"') do set today=%%i
+
+::Set log file name based on date
+set logFile=%logsPath%\backupRuns_%today%.log
+
+::Clean logs older than 7 days
+forfiles /p %logsPath% /m *.log /d -7 /c "cmd /c del @path" >nul 2>&1
 
 ::Path where backups are being saved (do not add the backslash "\" or it will fail)
 set backupDir=H:
 
+:: = Backup =
 ::Search for directory beginning with DayZServer_%today%
 dir /b "%backupDir%\DayZServer_%today%*" >nul 2>&1
 if errorlevel 1 (
     echo No backup found for today. Starting the backup script . . .
+    >> "%logFile%" echo.
+    >> "%logFile%" echo ------------------------------------------------------------------------------------------
+    >> "%logFile%" echo.
+    >> "%logFile%" echo [*] -- [ !date! @ !time! ] - Backup script executed.
 
     :: !!! You must have 7-Zip installed for the backup script to work !!!
 
     ::Change the path if needed
     pwsh -File "H:\backup_7z.ps1"
+
+    >> "%logFile%" echo [+] -- [ !date! @ !time! ] - Backup script finished.
+    >> "%logFile%" echo.
+    >> "%logFile%" echo ------------------------------------------------------------------------------------------
+    >> "%logFile%" echo.
 ) else (
     echo Backup already exists for today. Skipping . . .
+    >> "%logFile%" echo [#] -- [ !date! @ !time! ] - Backup script skipped. Backup for today exists.
 )
+
+endlocal
 
 ::Time in seconds to wait before starting server again
 timeout 10
